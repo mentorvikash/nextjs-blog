3f629f3a1752123806681553c10f81993f8b2bed
mentorvikash <mentorvikash@gmail.com>
2022-06-26 18:40:01 +0530

    change the title of my site

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Vikash Singh';
+const name = 'High Income Coding';
 export const siteTitle = 'High Income Coding';
 
 export default function Layout({ children, home }) {
